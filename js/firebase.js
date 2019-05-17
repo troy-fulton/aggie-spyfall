7739891bd2db510e50aa7c7169b308c894fde40b
Jsbbvk <Jsbbvk@gmail.com>
2019-05-16 17:51:15 -0700

    ux

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCfyr3GbrCphfpOlZPpzN8KEb40Md3bDCU",
+    authDomain: "spyfall-b2c57.firebaseapp.com",
+    databaseURL: "https://spyfall-b2c57.firebaseio.com",
+    projectId: "spyfall-b2c57",
+    storageBucket: "spyfall-b2c57.appspot.com",
+    messagingSenderId: "1055304348759",
+    appId: "1:1055304348759:web:b662f2e97f456ac9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
--- js/gameUX.js
+++ js/gameUX.js
@@ -1,9 +1,11 @@
 
 
 $(".player").on("click", function(e) {
+    e.preventDefault();
     $(this).hasClass("strikethrough")? $(this).removeClass("strikethrough"): $(this).addClass("strikethrough");
 });
 
 $(".locationC").on("touchstart click", function(e) {
+    e.preventDefault();
     $(this).hasClass("strikethrough")? $(this).removeClass("strikethrough"): $(this).addClass("strikethrough");
 });
