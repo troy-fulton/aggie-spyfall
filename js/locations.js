c09fb00ca6a73746e1de6261dde3f29d9e01da10
Troy <troyfulton@tamu.edu>
2020-03-13 17:02:17 -0500

    Initial Aggie Commit

--- js/locations.js
+++ js/locations.js
@@ -2,115 +2,16 @@
 
 var generalLocation = [
     {
-        location: "School",
-        roles: ["Teacher", "Principal", "Lunch Lady",
-            "Club President", "Class President", "Janitor", "Nurse",
-            "Class Photographer", "Counselor", "Student"]
+        location: "Kyle Field",
+        roles: ["Yell Leader", "Reveille", "Quarterback", "Announcer", 
+        "Band Member", "Reveille's Handler", "Ol' Rock, the Good Ag",
+        "Photographer", "Parent", "Current Student", "Former Student"]
     },
     {
-        location: "Tropical Island",
-        roles: ["Coconut Farmer", "Truck Driver", "Surfer", "Shop Owner", "Tourist",
-        "Tour Guide", "Smuggler", "Shark", "Turtle", "Boat Owner"]
-    },
-    {
-        location: "Circus",
-        roles: ["Juggler", "Fire Breather", "Acrobat", "Pickpocket", "Child", "Vendor",
-        "Ring Master", "Clown", "Lion Tamer", "Weight Lifter"]
-    },
-    {
-        location: "Prison",
-        roles:["Guard", "Cook", "Warden", "Janitor", "Convict", "Security", "Visitor",
-        "Gangster", "FBI Agent", "Nurse"]
-    },
-    {
-        location: "Beach",
-        roles:["Child", "Thief", "Police Officer", "Surfer", "Tourist"
-        , "Shop Owner", "Street Vendor", "Boat Owner", "Shark", "Ice Cream Man"]
-    },
-    {
-        location: "Bar",
-        roles: ["Drunk Person", "Bartender", "Customer", "Undercover Cop", "Thug",
-        "Bouncer", "Drug Dealer", "Singer", "Entertainer"]
-    },
-    {
-      location: "Cargo Ship",
-      roles: ["Pirate", "Mechanic", "Captain", "Navigator", "Crewman", "Cook", "Engineer",
-              "Radioman", "Smuggler", "Worker"]
-    },
-    {
-      location:"Airplane",
-      roles:["First Class Passenger", "Pilot", "Flight Attendant", "Crying Baby",
-            "Old Man", "Air Marshall", "Businessman", "Smuggler", "FBI Agent", "Passenger"]
-    },
-    {
-      location: "Hotel",
-      roles: ["Maid", "Manager", "Janitor", "Receptionist", "Room Service", "Bellboy", "Businessman", "Tourist"
-            , "Cook", "Valet"]
-    },
-    {
-      location: "Construction Site",
-      roles: ["Welder", "Manager", "Bulldozer Driver", "Crane Operator", "Civilian"
-            , "Electrician", "Worker", "Architect", "Engineer", "Truck Driver"]
-    },
-    {
-      location: "Gas Station",
-      roles: ["Biker", "Cyclist", "Cashier", "Customer", "Homeless Man", "Truck Driver",
-            "Police Officer", "Drug Dealer", "Stray Dog", "Taxi Driver"]
-    },
-    {
-      location: "Cloud",
-      roles:["Care Bear", "Bird","Rainbow", "Carl Fredrickson", "Sun", "Zeus",
-            "Superman", "Lightning", "Balloon", "Mary Poppins"]
-    },
-    {
-      location: "Atlantis",
-      roles:["Atlantean", "Aquaman", "Poseidon", "Mermaid", "Neptune", "Council Member",
-            "Plankton", "Mermaid Man", "Scuba Diver", "Shark"]
-    },
-    {
-      location: "Rainforest",
-      roles:["Lemur Mort", "Jaguar", "Indiana Jones", "Native", "Cannibal", "Dora the Explorer",
-            "Monkey", "Mosquito", "King Julien", "Skeleton"]
-    },
-    {
-      location: "Suburb",
-      roles:["Homeowner", "Real Estate Agent", "Dog", "Police Officer", "Parent",
-            "Mayor", "City Council Member", "Garbage Truck Driver", "Gardener", "Drug Dealer"]
-    },
-    {
-      location: "Space Station",
-      roles:["Doctor", "Alien", "Astronaut", "Researcher", "Electrician", "Scientist",
-            "Radioman", "Asteroid", "Engineer", "Pilot"]
-    },
-    {
-      location: "Library",
-      roles:["Bookworm", "Librarian", "Teenager", "Parent", "Child", "Mayor",
-            "College Student", "Janitor", "Counselor", "Chess Tutor", "Tutor"]
-    },
-    {
-      location: "Mariana Trench",
-      roles:["Kraken", "Godzilla", "Megalodon", "Leviathan", "Jaws", "Moby Dick", "Sea Serpent",
-            "Goblin Shark", "Angular Fish", "Gulper Eel", "Vampire Squid", "Dumbo Octopus"]
-    },
-    {
-      location: "Train",
-      roles:["Passenger", "Engineer", "Conductor", "Businessman", "First Class Passenger", "Stowaway", "Service Attendant",
-            "Trainman", "Brakeman", "Crew Member"]
-    },
-    {
-      location: "National Park",
-      roles:["Park Ranger", "Tourist", "Hiker", "Camper", "Maintenance Worker",
-            "Equipment Operator", "First Aid Personnel", "Vehicle Operator", "Park Guide", "Utility Operator"]
-    },
-    {
-      location: "Ski Resort",
-      roles:["Skier", "Lift Operator", "Ski Instructor", "Lodger", "First Aid Personnel", "Equipment Mechanic",
-            "Ski Patroller", "Food Vendor", "Manager", "Snowboarder"]
-    },
-    {
-      location: "Music Festival",
-      roles:["Pop Star", "Dancer", "Audio Technician", "DJ", "Teenager", "Bartender",
-            "Backup Singer", "Security", "Supervisor", "Limousine Driver"]
+        location: "Sbisa Dining Hall",
+        roles: ["Server", "Front Desk Staff Member", "Current Student", 
+        "Prospective Student", "Custodian", "Ol' Rock, the Good Ag",
+        "Manager", "Someone Stealing Food"]
     }
 ];
 
